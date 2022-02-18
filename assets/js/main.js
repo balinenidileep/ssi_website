19ece332637c7b891f97fc0982b65dca516dfe47
Dileep Balineni <dileepb@ipsgsystems.com>
2022-02-18 17:12:16 +0530

    Removed unwanted files;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
-
+/**
+* Template Name: Vesperr - v4.7.0
+* Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
   "use strict";
 
